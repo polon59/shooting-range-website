89d94e52a4228b8fccef7bbb2c391923bbf733c0
polon59 <przemyslaw.polczak@gmail.com>
2018-07-14 22:27:37 +0200

    create 'scripts' folder for .js files

---
+++ scripts/s1.js
@@ -0,0 +1,17 @@
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+  
+  function validate() {
+    var email = $("#email").val();
+  
+    if (validateEmail(email)) {
+      document.getElementById("result").innerHTML = "dobre";
+    } else {
+        document.getElementById("result").innerHTML = "chujowe";
+    }
+    return false;
+  }
+
+  
